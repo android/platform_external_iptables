85e4f901538997819d93c6a37a02633f6f189c2e
Bernhard Rosenkränzer <Bernhard.Rosenkranzer@linaro.org>
2015-05-20 15:42:33 +0200

    Set -fno-strict-aliasing here so we can get rid of it globally
    Add -fno-strict-aliasing to the compiler flags where it is actually
    needed so we can get rid of the flag globally -- without it, compilers
    can optimize better.
    
    Change-Id: I0b4bb97b9aeb25cf7f92ed1c52b76f6a84fb77e1
    Signed-off-by: Bernhard Rosenkränzer <Bernhard.Rosenkranzer@linaro.org>

--- extensions/libext.mk
+++ extensions/libext.mk
@@ -23,6 +23,8 @@ LOCAL_CFLAGS+=-D_LARGEFILE_SOURCE=1 -D_LARGE_FILES -D_FILE_OFFSET_BITS=64 -D_REE
 # Accommodate arm-eabi-4.4.3 tools that don't set __ANDROID__
 LOCAL_CFLAGS+=-D__ANDROID__
 LOCAL_CFLAGS += $(MY_warnings)
+# for CONNMARK and conntrack modules
+LOCAL_CFLAGS += -fno-strict-aliasing
 
 MY_GEN_INITEXT:= $(MY_gen)/initext.c
 $(MY_GEN_INITEXT): MY_initext_func := $(addprefix $(libext_prefix)_,$(libext_build_mod))
